d94c5eb04c92e84206f6577e9e054fbcb7731002
Ben Staddon <quadraxis@hotmail.co.uk>
2014-04-30 12:13:16 +0100

    Update advanced settings panel

--- src/client/gui/settings/AdvancedSettings.java
+++ src/client/gui/settings/AdvancedSettings.java
@@ -8,9 +8,10 @@ import java.awt.event.ActionListener;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 import java.util.ArrayList;
+import java.util.List;
 
 import javax.swing.BorderFactory;
-import javax.swing.BoxLayout;
+import javax.swing.GroupLayout;
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
 import javax.swing.JComboBox;
@@ -26,21 +27,21 @@ import javax.swing.border.TitledBorder;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 
-import common.FS2Constants;
-import common.Logger;
-import common.Util;
-
 import client.ClientExecutor;
 import client.gui.JBytesBox;
 import client.gui.MainFrame;
 import client.gui.MainFrame.StatusHint;
 import client.indexnode.internal.InternalIndexnodeManager;
-import client.platform.Relauncher;
 import client.platform.ClientConfigDefaults.CK;
+import client.platform.Relauncher;
+
+import common.FS2Constants;
+import common.Logger;
+import common.Util;
 
 @SuppressWarnings("serial")
 public class AdvancedSettings extends SettingsPanel {
-
+	
 	InternalIndexnodeManager iim = frame.getGui().getShareServer().getIndexNodeCommunicator().getInternalIndexNode();
 	
 	public AdvancedSettings(MainFrame frame) {
@@ -48,7 +49,7 @@ public class AdvancedSettings extends SettingsPanel {
 		
 		JPanel boxes = createScrollableBoxlayout();
 		
-		//###### actual items go here:
+		// ###### Actual items go here:
 		JLabel warning = new JLabel("<html>\"<b>You're probably going to break anything you change here</b>\"<br><i>--Captain Obvious</i></html>", frame.getGui().getUtil().getImage("failure"), SwingConstants.LEFT);
 		warning.setAlignmentX(CENTER_ALIGNMENT);
 		boxes.add(warning);
@@ -58,10 +59,9 @@ public class AdvancedSettings extends SettingsPanel {
 		boxes.add(autoindexnodePanel());
 		boxes.add(portPanel());
 		boxes.add(resetToDefaultsPanel());
-		//###### no more items.
-		
+		// ###### No more items.
 	}
-
+	
 	JLabel autoindexInfo;
 	
 	private JPanel autoindexnodePanel() {
@@ -111,102 +111,83 @@ public class AdvancedSettings extends SettingsPanel {
 			status = "disabled";
 		}
 		
-		String positionInfo = (iim.getRank()!=0 && (status.equals("inactive") || status.equals("active")) ? "<br>Our automatic indexnode rank is <b>"+iim.getRank()+"</b> out of <b>"+iim.getAlternativeNodes()+"</b>." : "");
+		String positionInfo = (iim.getRank() != 0 && (status.equals("inactive") || status.equals("active")) ? "<br>Our automatic indexnode rank is <b>" + iim.getRank() + "</b> out of <b>" + iim.getAlternativeNodes() + "</b>." : "");
 		
 		autoindexInfo.setText("<html>The internal indexnode is: <b>" + status + "</b>" + positionInfo + "</html>");
 	}
 	
 	private JPanel createSlotsPanel() {
-		JPanel ret = new JPanel(new BorderLayout());
-		ret.setBorder(getTitledBoldBorder("Transfer slots"));
+		JPanel slotsPanel = new JPanel(new FlowLayout(FlowLayout.LEADING));
+		slotsPanel.setBorder(getTitledBoldBorder("Transfer slots"));
 		
-		JPanel inner = new JPanel();
-		ret.add(inner, BorderLayout.NORTH);
+		JPanel innerPanel = new JPanel();
+		GroupLayout layout = new GroupLayout(innerPanel);
+		layout.setAutoCreateGaps(true);
+		innerPanel.setLayout(layout);
 		
-		inner.setLayout(new BoxLayout(inner, BoxLayout.PAGE_AXIS));
-		
-		inner.add(doUploadSlotsSection());
-		inner.add(doDownloadSlotsSection());
-		
-		return ret;
-	}
-
-	private JPanel doUploadSlotsSection() {
-		JPanel upload = new JPanel();
-		upload.setLayout(new BoxLayout(upload, BoxLayout.X_AXIS));
-		upload.setBorder(BorderFactory.createEmptyBorder(0, 0, 5, 0));
+		JLabel upLabel = new JLabel("<html><b>Upload:</b></html>");
 		
 		final JSpinner upMaxSlots = new JSpinner(new SpinnerNumberModel(frame.getGui().getShareServer().getUploadSlots(), 1, Integer.MAX_VALUE, 1));
-		final JSpinner upMaxSlotsPerClient = new JSpinner(new SpinnerNumberModel(frame.getGui().getShareServer().getUploadSlotsPerUser(), 1, Integer.MAX_VALUE, 1));
-		
 		upMaxSlots.addChangeListener(new ChangeListener() {
 			@Override
 			public void stateChanged(ChangeEvent e) {
-				frame.getGui().getShareServer().setUploadSlots((Integer)upMaxSlots.getValue());
+				frame.getGui().getShareServer().setUploadSlots((Integer) upMaxSlots.getValue());
 			}
 		});
+		registerHint(upMaxSlots, new StatusHint(frame.getGui().getUtil().getImage("upload"), "The maximum number of concurrent uploads"));
+		((DefaultEditor) upMaxSlots.getEditor()).getTextField().setColumns(3);
+		
+		JLabel upPerClientLabel = new JLabel("per user:");
+		
+		final JSpinner upMaxSlotsPerClient = new JSpinner(new SpinnerNumberModel(frame.getGui().getShareServer().getUploadSlotsPerUser(), 1, Integer.MAX_VALUE, 1));
 		upMaxSlotsPerClient.addChangeListener(new ChangeListener() {
 			@Override
 			public void stateChanged(ChangeEvent e) {
-				frame.getGui().getShareServer().setUploadSlotsPerUser((Integer)upMaxSlotsPerClient.getValue());
+				frame.getGui().getShareServer().setUploadSlotsPerUser((Integer) upMaxSlotsPerClient.getValue());
 			}
 		});
-		
-		registerHint(upMaxSlots, new StatusHint(frame.getGui().getUtil().getImage("upload"), "The maximum number of concurrent uploads"));
 		registerHint(upMaxSlotsPerClient, new StatusHint(frame.getGui().getUtil().getImage("upload"), "The number of slots a single peer can use at once"));
-		
-		((DefaultEditor) upMaxSlots.getEditor()).getTextField().setColumns(3);
 		((DefaultEditor) upMaxSlotsPerClient.getEditor()).getTextField().setColumns(3);
 		
-		JLabel uploadl = new JLabel("<html><b>Upload:</b></html>");
-		uploadl.setBorder(BorderFactory.createEmptyBorder(0,0,0,5));
-		upload.add(uploadl);
-		upload.add(upMaxSlots);
-		upload.add(new JLabel("   per user:"));
-		upload.add(upMaxSlotsPerClient);
-		
-		JPanel lefter = new JPanel(new BorderLayout());
-		lefter.add(upload, BorderLayout.WEST);
-		return lefter;
-	}
-	
-	private JPanel doDownloadSlotsSection() {
-		JPanel dl = new JPanel();
-		dl.setLayout(new BoxLayout(dl, BoxLayout.X_AXIS));
-		dl.setBorder(BorderFactory.createEmptyBorder(0, 0, 5, 0));
+		JLabel downLabel = new JLabel("<html><b>Download:</b></html>");
 		
 		final JSpinner downMaxSlots = new JSpinner(new SpinnerNumberModel(frame.getGui().getDc().getDownloadSlots(), 1, Integer.MAX_VALUE, 1));
-		final JSpinner downMaxParts = new JSpinner(new SpinnerNumberModel(frame.getGui().getDc().getMaxSlotsPerFile(), 1, Integer.MAX_VALUE, 1));
-		
 		downMaxSlots.addChangeListener(new ChangeListener() {
 			@Override
 			public void stateChanged(ChangeEvent e) {
-				frame.getGui().getDc().setDownloadSlots((Integer)downMaxSlots.getValue());
+				frame.getGui().getDc().setDownloadSlots((Integer) downMaxSlots.getValue());
 			}
 		});
+		registerHint(downMaxSlots, new StatusHint(frame.getGui().getUtil().getImage("download"), "The maximum number of concurrent downloads"));
+		((DefaultEditor) downMaxSlots.getEditor()).getTextField().setColumns(3);
+		
+		JLabel downPartsLabel = new JLabel("per file:");
+		
+		final JSpinner downMaxParts = new JSpinner(new SpinnerNumberModel(frame.getGui().getDc().getMaxSlotsPerFile(), 1, Integer.MAX_VALUE, 1));
 		downMaxParts.addChangeListener(new ChangeListener() {
 			@Override
 			public void stateChanged(ChangeEvent e) {
-				frame.getGui().getDc().setMaxSlotsPerFile((Integer)downMaxParts.getValue());
+				frame.getGui().getDc().setMaxSlotsPerFile((Integer) downMaxParts.getValue());
 			}
 		});
-		
-		registerHint(downMaxSlots, new StatusHint(frame.getGui().getUtil().getImage("download"), "The maximum number of concurrent downloads"));
 		registerHint(downMaxParts, new StatusHint(frame.getGui().getUtil().getImage("download"), "The maximum number of chunks a single file can be split into for downloading."));
-		
-		((DefaultEditor) downMaxSlots.getEditor()).getTextField().setColumns(3);
 		((DefaultEditor) downMaxParts.getEditor()).getTextField().setColumns(3);
 		
-		JLabel dll = new JLabel("<html><b>Download:</b></html>");
-		dll.setBorder(BorderFactory.createEmptyBorder(0,0,0,5));
-		dl.add(dll);
-		dl.add(downMaxSlots);
-		dl.add(new JLabel("   per file:"));
-		dl.add(downMaxParts);
+		GroupLayout.SequentialGroup hGroup = layout.createSequentialGroup();
+		hGroup.addGroup(layout.createParallelGroup().addComponent(upLabel).addComponent(downLabel));
+		hGroup.addGroup(layout.createParallelGroup().addComponent(upMaxSlots).addComponent(downMaxSlots));
+		hGroup.addGroup(layout.createParallelGroup().addComponent(upPerClientLabel).addComponent(downPartsLabel));
+		hGroup.addGroup(layout.createParallelGroup().addComponent(upMaxSlotsPerClient).addComponent(downMaxParts));
+		layout.setHorizontalGroup(hGroup);
 		
-		JPanel lefter = new JPanel(new BorderLayout());
-		lefter.add(dl, BorderLayout.WEST);
-		return lefter;
+		GroupLayout.SequentialGroup vGroup = layout.createSequentialGroup();
+		vGroup.addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE).addComponent(upLabel).addComponent(upMaxSlots).addComponent(upPerClientLabel).addComponent(upMaxSlotsPerClient));
+		vGroup.addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE).addComponent(downLabel).addComponent(downMaxSlots).addComponent(downPartsLabel).addComponent(downMaxParts));
+		layout.setVerticalGroup(vGroup);
+		
+		slotsPanel.add(innerPanel);
+		
+		return slotsPanel;
 	}
 	
 	JLabel heapInfo = new JLabel();
@@ -217,32 +198,31 @@ public class AdvancedSettings extends SettingsPanel {
 		content.setLayout(new BorderLayout());
 		content.setBorder(getTitledBoldBorder("Maximum heap size"));
 		
-		//Runtime.getRuntime().maxMemory()<(conf.getLong(CK.HEAPSIZE)*0.9
 		final JBytesBox heapsize = new JBytesBox(frame.getGui().getConf().getLong(CK.HEAPSIZE));
 		
 		heapsize.addPropertyChangeListener("value", new PropertyChangeListener() {
 			@Override
 			public void propertyChange(PropertyChangeEvent evt) {
 				long nv = (Long) evt.getNewValue();
-				if (nv<0) {
-					frame.setStatusHint(new StatusHint(SettingsTab.ERROR, "FS2's heap size can't be set to '"+heapsize.getText()+"'."));
-				} else {
-					if (nv > 32*1024*1024) {
-						frame.getGui().getConf().putLong(CK.HEAPSIZE, nv);
-						frame.setStatusHint(new StatusHint(SettingsTab.TICK, "FS2's heap set to "+Util.niceSize(nv)+", click 'Restart FS2' to apply changes"));
-						frame.getGui().getConf().putBoolean(CK.AUTO_HEAP_KNOWLEDGE, true); //suppress the "i've changed your heap for you" message.
-						restartNeeded();
-					} else {
-						frame.setStatusHint(new StatusHint(SettingsTab.ERROR, "The heap must be at least 32MiB."));
-					}
+				if (nv < 0) {
+					frame.setStatusHint(new StatusHint(SettingsTab.ERROR, "FS2's heap size can't be set to '" + heapsize.getText() + "'."));
+					return;
 				}
+				if (nv < 32 << 20) {
+					frame.setStatusHint(new StatusHint(SettingsTab.ERROR, "The heap must be at least 32MiB."));
+					return;
+				}
+				frame.getGui().getConf().putLong(CK.HEAPSIZE, nv);
+				frame.setStatusHint(new StatusHint(SettingsTab.TICK, "FS2's heap set to " + Util.niceSize(nv) + ", click 'Restart FS2' to apply changes"));
+				// Suppress the "I've changed your heap for you" message.
+				frame.getGui().getConf().putBoolean(CK.AUTO_HEAP_KNOWLEDGE, true);
+				restartNeeded();
 			}
 		});
 		
-		
 		content.add(heapInfo, BorderLayout.WEST);
 		setHeapInfo();
-		heapInfo.setBorder(BorderFactory.createEmptyBorder(0,0,0,5));
+		heapInfo.setBorder(BorderFactory.createEmptyBorder(0, 0, 0, 5));
 		content.add(heapsize, BorderLayout.CENTER);
 		
 		infoTimer = new Timer(5000, new ActionListener() {
@@ -292,13 +272,13 @@ public class AdvancedSettings extends SettingsPanel {
 		port.addChangeListener(new ChangeListener() {
 			@Override
 			public void stateChanged(ChangeEvent e) {
-				frame.getGui().getConf().putInt(CK.PORT, ((Integer)port.getValue()));
+				frame.getGui().getConf().putInt(CK.PORT, ((Integer) port.getValue()));
 				setPortNumberInfo();
 				restartNeeded();
 			}
 		});
 		
-		registerHint(port, new StatusHint(null, "The port that FS2 listens on. (port+1 is also used!)"));
+		registerHint(port, new StatusHint(frame.getGui().getUtil().getImage("network"), "The port that FS2 listens on. (port+1 is also used!)"));
 		
 		((DefaultEditor) port.getEditor()).getTextField().setColumns(7);
 		JPanel portShrinker = new JPanel(new BorderLayout());
@@ -312,21 +292,20 @@ public class AdvancedSettings extends SettingsPanel {
 	}
 	
 	private void setPortNumberInfo() {
-		ArrayList<String> ports = new ArrayList<String>();
-		ports.add(frame.getGui().getConf().getString(CK.PORT));
-		ports.add(Integer.toString(frame.getGui().getConf().getInt(CK.PORT) + 1));
-		ports.add(Integer.toString(FS2Constants.ADVERTISMENT_DATAGRAM_PORT));
-		ports.add(Integer.toString(FS2Constants.ADVERTISMENT_DATAGRAM_PORT + 1));
+		List<Integer> ports = new ArrayList<Integer>();
+		ports.add(frame.getGui().getConf().getInt(CK.PORT));
+		ports.add(frame.getGui().getConf().getInt(CK.PORT) + 1);
+		ports.add(FS2Constants.ADVERTISEMENT_DATAGRAM_PORT);
+		ports.add(FS2Constants.ADVERTISEMENT_DATAGRAM_PORT + 1);
 		if (iim.isCurrentlyActive()) {
-			ports.add(Integer.toString(iim.getPort()));
-			ports.add(Integer.toString(iim.getPort() + 1));
+			ports.add(iim.getPort());
+			ports.add(iim.getPort() + 1);
 		}
-		
-		portNumberInfo.setText("<html>FS2 is currently using ports: <b>" + Util.join(ports.toArray(), ", ") + "</b><br>Open these ports on your firewall to use FS2</html>");
+		portNumberInfo.setText("<html>FS2 is currently using ports: <b>" + Util.join(ports.toArray(), ", ") + "</b><br>Open these ports on your firewall to use FS2.</html>");
 	}
 	
 	/**
-	 * configures autoupdate settings, triggers a check for updates right now.
+	 * Configures autoupdate settings, triggers a check for updates right now.
 	 * @return
 	 */
 	private JPanel autoupdatePanel() {
@@ -341,7 +320,7 @@ public class AdvancedSettings extends SettingsPanel {
 			choice.setSelectedIndex(2);
 		} else if (frame.getGui().getConf().getString(CK.UPDATE_POLICY).equals("ask")) {
 			choice.setSelectedIndex(1);
-		} else if (frame.getGui().getConf().getString(CK.UPDATE_POLICY).equals("auto")) { //pointless test for completeness.
+		} else if (frame.getGui().getConf().getString(CK.UPDATE_POLICY).equals("auto")) { // Pointless test for completeness.
 			choice.setSelectedIndex(0);
 		}
 		
@@ -362,7 +341,7 @@ public class AdvancedSettings extends SettingsPanel {
 			}
 		});
 		
-		JLabel auinfo = new JLabel("<html>Select <i>'"+options[2]+"'</i> to prevent FS2 from even checking for updates.</html>");
+		JLabel auinfo = new JLabel("<html>Select <i>'" + options[2] + "'</i> to prevent FS2 from even checking for updates.</html>");
 		ret.add(auinfo, BorderLayout.NORTH);
 		ret.add(choice, BorderLayout.WEST);
 		
@@ -380,14 +359,14 @@ public class AdvancedSettings extends SettingsPanel {
 	
 	private void restartNeeded() {
 		restartFS2.setText(restartFS2.getText().toUpperCase());
-		restartFS2.setBackground(JBytesBox.bad);
-		buttonsPanel.setBackground(JBytesBox.bad);
-		restartFS2.setFont(restartFS2.getFont().deriveFont(Font.BOLD|Font.ITALIC));
-		((TitledBorder)buttonsPanel.getBorder()).setTitle("You need to restart FS2 to apply the changes!");
+		restartFS2.setBackground(JBytesBox.BAD);
+		buttonsPanel.setBackground(JBytesBox.BAD);
+		restartFS2.setFont(restartFS2.getFont().deriveFont(Font.BOLD | Font.ITALIC));
+		((TitledBorder) buttonsPanel.getBorder()).setTitle("You need to restart FS2 to apply the changes!");
 	}
 	
 	private JButton resetFS2;
-	private JButton restartFS2; //clicking this restarts the FS2 client.
+	private JButton restartFS2; // Clicking this restarts the FS2 client.
 	private JPanel buttonsPanel;
 	
 	/**
@@ -402,7 +381,8 @@ public class AdvancedSettings extends SettingsPanel {
 		restartFS2.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				if (!Relauncher.increaseHeap(frame.getGui().getConf().getLong(CK.HEAPSIZE), false)) { //this is done by restarting to a new heap of possibly the same size.
+				// This is done by restarting to a new heap of possibly the same size.
+				if (!Relauncher.increaseHeap(frame.getGui().getConf().getLong(CK.HEAPSIZE), false)) {
 					JOptionPane.showMessageDialog(null, "The client couldn't be restarted. Restart is only supported from .jar files.", "Restart failure.", JOptionPane.ERROR_MESSAGE);
 				}
 			}
