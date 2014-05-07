89072bd97d5ac278870245836ccc516ea1d8ccab
Ben Staddon <quadraxis@hotmail.co.uk>
2014-05-07 05:55:32 +0100

    Add icon to status bar

--- src/client/gui/StatusBar.java
+++ src/client/gui/StatusBar.java
@@ -14,19 +14,19 @@ import javax.swing.JPanel;
 import javax.swing.Timer;
 import javax.swing.border.EtchedBorder;
 
-import client.gui.TabItem.FS2Tab;
-
 import common.FS2Constants;
 import common.Util;
 
+import client.gui.TabItem.FS2Tab;
+
 /**
  * This is the status bar for FS2. 
- * @author gary
+ * @author Gary
  */
 @SuppressWarnings("serial")
 public class StatusBar extends JPanel implements ActionListener, MouseListener {
 	
-	JLabel infoText = new JLabel("Welcome to FS2...");
+	JLabel infoText;
 	JLabel upInfo;
 	JLabel downInfo;
 	JLabel chatInfo;
@@ -38,20 +38,21 @@ public class StatusBar extends JPanel implements ActionListener, MouseListener {
 		
 		this.frame = frame;
 		
+		infoText = new JLabel("Welcome to FS2...", frame.gui.util.getImage("info"), JLabel.LEFT);
 		infoText.setBorder(BorderFactory.createEmptyBorder(2, 2, 2, 2));
-		add(infoText,BorderLayout.WEST);
+		add(infoText, BorderLayout.WEST);
 		
-		JPanel statsArea = new JPanel(new FlowLayout(FlowLayout.RIGHT,5,2));
-		
+		JPanel statsArea = new JPanel(new FlowLayout(FlowLayout.RIGHT, 5, 2));
 		add(statsArea, BorderLayout.EAST);
 		
-		chatInfo = new JLabel("0", frame.gui.getUtil().getImage("chat"), JLabel.LEFT);
+		chatInfo = new JLabel("0", frame.gui.util.getImage("chat"), JLabel.LEFT);
 		chatInfo.addMouseListener(this);
 		statsArea.add(chatInfo);
 		
 		upInfo = new JLabel("uploads info", frame.gui.util.getImage("upload"), JLabel.LEFT);
 		upInfo.addMouseListener(this);
 		statsArea.add(upInfo);
+		
 		downInfo = new JLabel("downloads info", frame.gui.util.getImage("download"), JLabel.LEFT);
 		downInfo.addMouseListener(this);
 		statsArea.add(downInfo);
@@ -62,58 +63,58 @@ public class StatusBar extends JPanel implements ActionListener, MouseListener {
 		updater.start();
 	}
 	
-	public void setInfoTextWithIcon(String info, Icon ico) {
+	public void setInfoTextWithIcon(String info, Icon icon) {
 		infoText.setText(info);
-		infoText.setIcon(ico);
+		infoText.setIcon(icon);
 	}
 	
 	public void setInfoText(String info) {
 		setInfoTextWithIcon(info, null);
 	}
-
+	
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		if (e.getSource()==updater) {
-			//1) speed labels:
-			String upStr = /*Util.niceSize( frame.gui.ssvr.getUploadTracker().getPosition(), true)+"@"+*/ Util.niceSize((long) frame.gui.ssvr.getUploadTracker().getSpeed(),true)+"/s ["+Util.niceSize(frame.gui.ssvr.getUploadSpeed(), true)+"/s]";
+		if (e.getSource() == updater) {
+			// 1) Speed labels:
+			String upStr = Util.niceSize((long) frame.gui.ssvr.getUploadTracker().getSpeed(), true) + "/s [" + Util.niceSize(frame.gui.ssvr.getUploadSpeed(), true) + "/s]";
 			upInfo.setText(upStr);
-			String downStr = /*Util.niceSize( frame.gui.dc.getDownloadTracker().getPosition(), true)+"@"+*/ Util.niceSize((long) frame.gui.dc.getDownloadTracker().getSpeed(),true)+"/s ["+Util.niceSize(frame.gui.dc.getDownloadSpeed(), true)+"/s]";
+			String downStr = Util.niceSize((long) frame.gui.dc.getDownloadTracker().getSpeed(), true) + "/s [" + Util.niceSize(frame.gui.dc.getDownloadSpeed(), true) + "/s]";
 			downInfo.setText(downStr);
 			
-			//2) chat label:
+			// 2) Chat label:
 			if (frame.getInstantiatedTabs().containsKey(FS2Tab.CHAT)) {
-				chatInfo.setText( Integer.toString(((ChatTab)frame.getInstantiatedTabs().get(FS2Tab.CHAT)).getUnreadMessageCount()) );
+				chatInfo.setText(Integer.toString(((ChatTab) frame.getInstantiatedTabs().get(FS2Tab.CHAT)).getUnreadMessageCount()));
 			}
 			
-			frame.trayicon.setPopupInfoString("Up: "+upStr+"    Down: "+downStr);
+			frame.trayicon.setPopupInfoString("Up: " + upStr + "    Down: " + downStr);
 		}
 	}
 	
 	/**
-	 * Shuts down the status bar, (stops the internal update timer)
+	 * Shuts down the status bar. (stops the internal update timer)
 	 */
 	void shutdown() {
 		updater.stop();
 	}
-
+	
 	@Override
 	public void mouseClicked(MouseEvent e) {
-		if (e.getSource()==chatInfo) {
+		if (e.getSource() == chatInfo) {
 			frame.openTab(FS2Tab.CHAT);
-		} else if (e.getSource()==upInfo || e.getSource()==downInfo) {
+		} else if (e.getSource() == upInfo || e.getSource() == downInfo) {
 			frame.openTab(FS2Tab.STATS);
 		}
 	}
-
+	
 	@Override
 	public void mouseEntered(MouseEvent e) {}
-
+	
 	@Override
 	public void mouseExited(MouseEvent e) {}
-
+	
 	@Override
 	public void mousePressed(MouseEvent e) {}
-
+	
 	@Override
 	public void mouseReleased(MouseEvent e) {}
 	
